c292fac8cf68f72501bfd32dc95b838ef077e474
Metche216 <drmatiasetcheverry@gmail.com>
2024-05-29 18:31:45 -0400

    creada tortuga creados los autos que se mueven

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+CAR_LIST = []
+CARS_QTY = 60
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color(random.choice(COLORS))
+        self.shape('square')
+        self.resizemode('user')
+        self.shapesize(1,2)
+        self.penup()
+        self.goto(x=340,y=random.randint(-220,220))
+        self.moving_speed = MOVE_INCREMENT*-1
+        CAR_LIST.append(self)
+        
+        
+        
+    def move(self):
+        self.goto(x=self.xcor() + self.moving_speed,y=self.ycor())
+        
+    
--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 import time
 from turtle import Screen
 from player import Player
-from car_manager import CarManager
+from car_manager import CarManager, CAR_LIST as CL, CARS_QTY as CQ
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player_1 = Player()
+
+
+
+screen.listen()
+
+screen.onkeypress(player_1.move, 'Up')
+
+
 game_is_on = True
 while game_is_on:
+    car_fire = random.randint(1,3)
     time.sleep(0.1)
     screen.update()
+    
+    if len(CL) < CQ:
+        if car_fire == 1:
+            car = CarManager()
+            
+            
+    
+    if player_1.ycor() >260:
+        player_1.reset()
+    
+    
+    
+    #CHECK IF CARRER ENDED
+    for car in CL:
+        if car.xcor() < -340:
+            CL.remove(car)
+            del car
+        else:
+            car.move()
+    
+    
